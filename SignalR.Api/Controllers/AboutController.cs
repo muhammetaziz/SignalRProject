df29c99fd545f5c11be5bfe0e5e3b89b23e44b3d
Muhammet Aziz AÇIKER <m.aziz.aciker@gmail.com>
2025-01-25 23:58:45 +0300

    mapper ve Dtoların yazılması apinin ayağa kaldırılması

---
+++ SignalR.Api/Controllers/AboutController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.AboutDto;
+
+namespace SignalR.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AboutController : ControllerBase
+    {
+        private readonly IAboutService _aboutService;
+
+        public AboutController(IAboutService aboutService)
+        {
+            _aboutService = aboutService;
+        }
+
+        [HttpGet] 
+        public IActionResult AboutList()
+        {
+            var values=_aboutService.TGetListAll();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public  IActionResult CreateAbout(CreateAboutDto createAbout)
+        {
+            if (ModelState.IsValid)
+            {
+                _aboutService.TAdd(createAbout);
+                return Created("", createAbout);
+            }
+        }
+            
+    }
+}
---
+++ SignalR.Api/Mapping/FeaturMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using SignalR.DtoLayer.FeatureDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalR.Api.Mapping
+{
+    public class FeaturMapping:Profile
+    {
+        public FeaturMapping()
+        {
+            CreateMap<Feature, ResultFeatureDto>().ReverseMap();
+            CreateMap<Feature, CreateFeatureDto>().ReverseMap();
+            CreateMap<Feature, UpdateFeatureDto>().ReverseMap();
+            CreateMap<Feature, GetFeatureDto>().ReverseMap();
+        }
+    }
+}
