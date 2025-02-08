16731412cf9bef38f9b4024345ddc6cc321ad7ef
Muhammet Aziz AÇIKER <m.aziz.aciker@gmail.com>
2025-02-09 00:49:19 +0300

    About Tamamlandı

--- SignalR.WebUI/Controllers/AboutController.cs
+++ SignalR.WebUI/Controllers/AboutController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using SignalR.WebUI.DTOs.AboutDtos;
+using System.Text;
 
 namespace SignalR.WebUI.Controllers
 {
@@ -26,5 +27,59 @@ namespace SignalR.WebUI.Controllers
             return View();
         }
 
+        [HttpGet]
+        public IActionResult CreateAbout()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateAbout(CreateAboutDto createAboutDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createAboutDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responceMessage = await client.PostAsync("https://localhost:7115/api/About", stringContent);
+            if (responceMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+        public async Task<IActionResult> DeleteAbout(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responceMessage = await client.DeleteAsync($"https://localhost:7115/api/About/{id}");
+            if (responceMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> UpdateAbout(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responceMessage = await client.GetAsync($"https://localhost:7115/api/About/{id}");
+            if (responceMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responceMessage.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<UpdateAboutDto>(jsonData);
+                return View(value);
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateAbout(UpdateAboutDto updateAboutDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateAboutDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responceMessage = await client.PutAsync("https://localhost:7115/api/About", stringContent);
+            if (responceMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
     }
 }
